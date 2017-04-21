8aefc1e36d03bfa2d96302de63b95e95aaa9e822
Axen Georget <axen.georget@epita.fr>
2017-04-21 14:53:11 +0200

    Add tag option to block

--- Assets/Scripts/MapConstructor/Block.cs
+++ Assets/Scripts/MapConstructor/Block.cs
@@ -7,22 +7,28 @@ namespace Assets.Script
 		private string type;
 		private Vector3 position;
 		private float rotation;
+
+		private string tag;
 		
-		public Block(BlockType type, Vector3 position, float rotation) : this (type + "_col", position, rotation){}
+		public Block(BlockType type, Vector3 position, float rotation, string tag = "") : this (type + "_col", position, rotation, tag){}
 		
-		public Block(string type, Vector3 position, float rotation)
+		public Block(string type, Vector3 position, float rotation, string tag = "")
 		{
 			this.type = type;
 			this.position = position;
 			this.rotation = rotation;
+			this.tag = tag;
 		}
 		
 		public void Place()
 		{
-			UnityEngine.Object.Instantiate(
+			GameObject obj = UnityEngine.Object.Instantiate(
 				Resources.Load(this.type),
 				this.position, 
-				Quaternion.Euler(0.0f, rotation, 0.0f));
+				Quaternion.Euler(0.0f, rotation, 0.0f)) as GameObject;
+
+			if (tag != "")
+				obj.tag = tag;
 		}
 	}
 	
