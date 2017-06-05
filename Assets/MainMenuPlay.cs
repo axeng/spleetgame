14052c3a132bacfae276b71cb9aa97c5b7b767f2
Hugo Tritsch <hugo@MacBook-Pro-de-Hugo.local>
2017-06-05 11:31:33 +0200

    work on main menu

---
+++ Assets/MainMenuPlay.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuPlay : MonoBehaviour {
+
+	public MovieTexture movTexture;
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Renderer> ().material.mainTexture = movTexture;
+		movTexture.Play ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
