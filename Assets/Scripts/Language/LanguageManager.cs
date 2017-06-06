a8c4c2f07478d9f47c07ec6ce2562808c818862b
Axen Georget <axen.georget@epita.fr>
2017-06-07 00:08:51 +0200

    Base for multi language management

---
+++ Assets/Scripts/Language/LanguageManager.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Xml.Serialization;
+using System.IO;
+
+namespace Assets.Script
+{
+	[XmlRoot("LanguageManager")]
+	public class LanguageManager
+	{
+
+		private static string g_filepath = Path.Combine(Application.dataPath, "Resources/Xml/Languages.xml");
+
+		[XmlEnum("currentLanguage")]
+		public Language currentLanguage;
+
+		[XmlArray("translations"), XmlArrayItem("translations")]
+		public List<Translation> translations;
+
+		public LanguageManager()
+		{
+			currentLanguage = Language.English;
+			translations = new List<Translation>();
+		}
+
+
+		public string GetString(string key)
+		{
+			return GetString(key, currentLanguage);
+		}
+
+		public string GetString(string key, Language language)
+		{
+			int index = FindKey(key);
+
+			if (index >= 0)
+				return translations[index].GetValue(language);
+
+			return "";
+		}
+
+
+		public void Add(Translation translation)
+		{
+			int index = FindKey(translation.key);
+
+			if (index >= 0)
+				translations[index] = translation;
+			else
+				translations.Add(translation);
+		}
+
+		public void Update(Translation translation)
+		{
+			int index = FindKey(translation.key);
+
+			if (index >= 0)
+				translations[index] = translation;
+		}
+
+		public void Remove(Translation translation)
+		{
+			Remove(translation.key);
+		}
+
+		public void Remove(string key)
+		{
+			translations.RemoveAt(FindKey(key));
+		}
+
+		public void Save()
+		{
+			XmlSerializer serializer = new XmlSerializer(typeof(LanguageManager));
+			using (FileStream stream = new FileStream(g_filepath, FileMode.Create))
+			{
+				serializer.Serialize(stream, this);
+			}
+		}
+
+		public static LanguageManager Load()
+		{
+			if (File.Exists(g_filepath))
+			{
+				XmlSerializer serializer = new XmlSerializer(typeof(LanguageManager));
+				using (FileStream stream = new FileStream(g_filepath, FileMode.Open))
+				{
+					return serializer.Deserialize(stream) as LanguageManager;
+				}
+			}
+			return new LanguageManager();
+		}
+
+		public int FindKey(string key)
+		{
+			for (int i = 0; i < translations.Count; i++)
+			{
+				if (key == translations[i].key)
+					return i;
+			}
+
+			return -1;
+		}
+	}
+
+	public enum Language
+	{
+		English,
+		French
+	}
+}
---
+++ Assets/Scripts/Language/SystemEditor.cs
@@ -0,0 +1,93 @@
+﻿#if (UNITY_EDITOR)
+using UnityEditor;
+#endif
+using UnityEngine;
+
+namespace Assets.Script
+{
+	public class SystemEditor
+#if (UNITY_EDITOR)
+	: EditorWindow
+#endif
+	{
+		private const float WINDOW_MIN_WIDTH = 800.0f;
+		private const float WINDOW_MIN_HEIGHT = 640.0f;
+
+		private LanguageManager m_manager;
+		private int m_index;
+
+#if (UNITY_EDITOR)
+		[MenuItem("Window/System Editor")]
+		public static void GetWindow()
+		{
+			SystemEditor window = EditorWindow.GetWindow<SystemEditor>("System", true);
+			window.minSize = new Vector2(WINDOW_MIN_WIDTH, WINDOW_MIN_HEIGHT);
+		}
+
+		private void OnEnable()
+		{
+			m_manager = LanguageManager.Load();
+			m_index = 0;
+		}
+
+		private void OnDisable()
+		{
+			m_manager.Save();
+		}
+
+		private void OnGUI()
+		{
+			EditorGUILayout.BeginVertical(GUILayout.ExpandWidth(true), GUILayout.ExpandHeight(true));
+			if (m_manager.translations.Count > 0)
+			{
+				EditorGUILayout.LabelField(string.Format("Translation {0} Out Of {1}", m_index + 1, m_manager.translations.Count));
+				m_manager.translations[m_index].key = EditorGUILayout.TextField("Key:", m_manager.translations[m_index].key);
+				m_manager.translations[m_index].english = EditorGUILayout.TextField("English:", m_manager.translations[m_index].english);
+				m_manager.translations[m_index].french = EditorGUILayout.TextField("French:", m_manager.translations[m_index].french);
+
+			}
+			else
+			{
+				EditorGUILayout.LabelField("No translation found ...");
+			}
+			EditorGUILayout.EndVertical();
+
+			EditorGUILayout.BeginVertical(GUILayout.ExpandWidth(true));
+			if (GUILayout.Button("New Entry"))
+			{
+				EditorGUI.FocusTextInControl(null);
+
+				m_manager.Add(new Translation());
+				m_index = m_manager.translations.Count - 1;
+			}
+			if (GUILayout.Button("Delete Entry") && m_manager.translations.Count > 0)
+			{
+				EditorGUI.FocusTextInControl(null);
+
+				m_manager.translations.RemoveAt(m_index);
+				m_index = Mathf.Clamp(m_index, 0, m_manager.translations.Count - 1);
+			}
+			EditorGUILayout.EndVertical();
+
+			EditorGUILayout.BeginHorizontal(GUILayout.ExpandWidth(true));
+			if (GUILayout.Button("Previous") && m_manager.translations.Count > 0)
+			{
+				EditorGUI.FocusTextInControl(null);
+
+				m_index--;
+				if (m_index < 0)
+					m_index = 0;
+			}
+			if (GUILayout.Button("Next") && m_manager.translations.Count > 0)
+			{
+				EditorGUI.FocusTextInControl(null);
+
+				m_index++;
+				if (m_index >= m_manager.translations.Count)
+					m_index = m_manager.translations.Count - 1;
+			}
+			EditorGUILayout.EndHorizontal();
+		}
+#endif
+	}
+}
