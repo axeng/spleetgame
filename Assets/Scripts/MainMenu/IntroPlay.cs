93522c719ee2619ef90235562669ba1c3659b908
Axen Georget <axen.georget@epita.fr>
2017-06-07 18:26:49 +0200

    Final Intro

--- Assets/Scripts/MainMenu/IntroPlay.cs
+++ Assets/Scripts/MainMenu/IntroPlay.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class IntroPlay : MonoBehaviour {
 
@@ -8,7 +9,7 @@ public class IntroPlay : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		GetComponent<Renderer> ().material.mainTexture = movTexture;
+		GetComponent<RawImage>().texture = movTexture;
 		movTexture.Play ();
 	}
 	
